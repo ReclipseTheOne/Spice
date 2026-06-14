0930020cc6e0fc3f812170f4e62040f8b25f3520
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2026-06-14 22:01:00 +0300

    v0.2.1 - Fix project naming

--- spice-lang/spice/__init__.py
+++ spice-lang/spice/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import version
 
 from spice import cli, lexer, parser, transformer, errors, printils, compilation
 
-__version__ = version("spicy")
+__version__ = version("spice-lang")
 
 __all__ = ["cli", "lexer", "parser", "transformer", "compilation", "errors", "printils"]
--- spice-lang/spice/cli/cli_handler.py
+++ spice-lang/spice/cli/cli_handler.py
@@ -17,7 +17,7 @@ import click
 @click.option('-v', '--verbose', is_flag=True, help='Verbose output')
 @click.option('-nf', '--no-final-check', is_flag=True, help='Skip final type checks at compilation')
 @click.option('--runtime-checks', is_flag=True, help='Add runtime type checking to output')
-@click.version_option(package_name='spicy', prog_name='spicy')
+@click.version_option(package_name='spice-lang', prog_name='spicy')
 def from_cli(source: str, output: Optional[str], emit: str, keep_intermidiates: bool, check: bool, watch: bool, verbose: bool, no_final_check: bool, runtime_checks: bool):
     """Compile Spice (.spc) files to Python, Cython, or standalone executables."""
     flags: CLI_FLAGS = CLI_FLAGS(
